0805ca16f730ce40f04f744b88380780a89ed97f
Wenke Yang <wenke_yang@amp.com.au>
2019-03-29 00:21:08 +1100

    add udf

--- src/main/scala/au/com/weather_simulator/Runtest.scala
+++ src/main/scala/au/com/weather_simulator/Runtest.scala
@@ -1,7 +1,10 @@
 package au.com.weather_simulator
 
+import java.util.Random
+
 import au.com.weather_simulator.utiles.SparkUtiles
 import utiles.SparkUtiles._
+import org.apache.spark.sql.functions.{col, row_number, udf}
 
 object Runtest {
   def main(args: Array[String]): Unit = {
@@ -9,9 +12,35 @@ object Runtest {
 
     implicit val spark = getSparkSession("localTest")
 
+    spark.udf.register("rand_temp", (rangeMin: Double, rangeMax: Double) => {
+      rangeMin + (rangeMax - rangeMin) * new Random().nextDouble()
+    })
+
     val sydney = SparkUtiles.readCSV("src/main/resources/sydney.csv")
-    sydney.show()
+    sydney.printSchema()
+    sydney.createOrReplaceTempView("sydney")
+
+
+    spark.sql(
+      """
+        |select monthday, maxtemp, mintemp ,
+        | cast(rand_temp(mintemp,maxtemp) as DECIMAL(8,1)) as emutemp
+        | from sydney
+      """.stripMargin).show
+
 
     spark.close()
+
+
+
+
+    //    val rangeMin = 2.2
+    //    val rangeMax = 3.5
+    //    val r = new Random()
+    //    var randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble()
+    //
+    //    for (x <- 1 until 100)
+    //      println(rangeMin + (rangeMax - rangeMin) * r.nextDouble())
+
   }
 }
